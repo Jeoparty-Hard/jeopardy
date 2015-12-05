af4741faf9ad9cfcc650e7559b07c2e554ab8a9b
Manuel Vögele <develop@manuel-voegele.de>
2015-12-05 02:21:06 +0100

    Game stub

---
+++ game.hpp
@@ -0,0 +1,21 @@
+#ifndef JEOPARDY_GAME_CPP_H
+#define JEOPARDY_GAME_CPP_H
+
+#include <list>
+
+class game
+{
+public:
+    enum class state
+    {
+        NEW,
+        SETUP,
+        SCOREBOARD,
+        ANSWER
+    };
+private:
+public:
+
+};
+
+#endif //JEOPARDY_GAME_CPP_H
