4affdc4cb9d4c8ddd8c2061bc75a88a97a424eae
Manuel Vögele <develop@manuel-voegele.de>
2015-12-06 00:49:00 +0100

    Basic websocket server implementation

--- websocket_server.cpp
+++ websocket_server.cpp
@@ -1,1 +1,62 @@
 #include "websocket_server.hpp"
+
+#include <functional>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+using namespace std;
+using namespace std::placeholders;
+using namespace rapidjson;
+using namespace websocketpp;
+
+websocket_server::websocket_server()
+{
+    this->init_asio();
+    this->set_open_handler(bind(&websocket_server::on_connection_open, this, _1));
+}
+
+void websocket_server::start_listen(int port)
+{
+    // TODO Reuse address
+    this->listen(port);
+    this->start_accept();
+}
+
+void websocket_server::on_message(connection_hdl, message_ptr)
+{
+    // TODO Implement
+}
+
+void websocket_server::on_connection_open(connection_hdl hdl)
+{
+    // TODO Handshake?
+    connections.insert(hdl);
+}
+
+void websocket_server::on_connection_close(connection_hdl hdl)
+{
+    connections.erase(hdl);
+}
+
+void websocket_server::broadcast(const Document &data)
+{
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    data.Accept(writer);
+    string json = buffer.GetString();
+    for (const connection_hdl &hdl : connections)
+    {
+        this->send(hdl, json, frame::opcode::value::TEXT);
+    }
+}
+
+void websocket_server::shutdown()
+{
+    // TODO Call in destructor
+    this->stop_listening();
+    for (const connection_hdl &hdl : connections)
+    {
+        this->close(hdl, 0, "Server shutting down");
+    }
+    this->stop();
+}
--- websocket_server.hpp
+++ websocket_server.hpp
@@ -3,13 +3,14 @@
 
 #include <set>
 
+#include <rapidjson/document.h>
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
 class websocket_server : public websocketpp::server<websocketpp::config::asio>
 {
 private:
-    std::set<websocketpp::connection_hdl> connections;
+    std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> connections;
     void on_connection_open(websocketpp::connection_hdl hdl);
     void on_connection_close(websocketpp::connection_hdl hdl);
     void on_message(websocketpp::connection_hdl, message_ptr);
@@ -17,6 +18,7 @@ public:
     websocket_server();
     void start_listen(int port);
     void shutdown();
+    void broadcast(const rapidjson::Document &);
 };
 
 #endif //JEOPARDY_WEBSOCKET_SERVER_H
