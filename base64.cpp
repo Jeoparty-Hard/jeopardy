9de83be2751db9aa58b2b6882f67979ff26fad10
Manuel Vögele <develop@manuel-voegele.de>
2015-12-05 18:43:59 +0100

    Base64 encoder

---
+++ base64.cpp
@@ -0,0 +1,75 @@
+#include "base64.hpp"
+
+using namespace std;
+
+static const string base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+typedef struct
+{
+    char byte1;
+    char byte2;
+    char byte3;
+} origdata;
+
+typedef struct
+{
+    unsigned char char1 : 6;
+    unsigned char char2 : 6;
+    unsigned char char3 : 6;
+    unsigned char char4 : 6;
+} b64data;
+
+typedef union
+{
+    origdata data;
+    b64data base64;
+} b64union;
+
+string base64encode(const vector<char> &data)
+{
+    char byte0;
+    char byte1;
+    char byte2;
+    unsigned char char0;
+    unsigned char char1;
+    unsigned char char2;
+    unsigned char char3;
+    int fillbytes = 0;
+    string result = "";
+    for (int i = 0;i < data.size();i += 3)
+    {
+        byte0 = data[i];
+        if (i + 1 < data.size())
+        {
+            byte1 = data[i + 1];
+            if (i + 2 < data.size())
+            {
+                byte2 = data[i + 2];
+            }
+            else
+            {
+                byte2 = 0;
+                fillbytes = 1;
+            }
+        }
+        else
+        {
+            byte1 = 0;
+            byte2 = 0;
+            fillbytes = 2;
+        }
+        char0 = byte0 >> 2;
+        char1 = (byte0 & 0b00000011) << 4 | (byte1 & 0b11110000) >> 4;
+        char2 = (byte1 & 0b00001111) << 2 | (byte2 & 0b11000000) >> 6;
+        char3 = byte2 & 0b00111111;
+        result += base64chars[char0];
+        result += base64chars[char1];
+        result += base64chars[char2];
+        result += base64chars[char3];
+    }
+    for (int i = 0;i < fillbytes;i++)
+    {
+        result[result.size() - i - 1] = '=';
+    }
+    return result;
+}
---
+++ base64.hpp
@@ -0,0 +1,10 @@
+#ifndef JEOPARDY_BASE64_H
+#define JEOPARDY_BASE64_H
+
+#include <string>
+#include <vector>
+
+
+std::string base64encode(const std::vector<char> &data);
+
+#endif //JEOPARDY_BASE64_H
