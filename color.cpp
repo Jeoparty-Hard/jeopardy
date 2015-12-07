90473187a3c001fc5dbf8a6841693eb74e7aa986
Manuel Vögele <develop@manuel-voegele.de>
2015-12-07 01:07:18 +0100

    Cache the colorstring

--- color.cpp
+++ color.cpp
@@ -16,10 +16,14 @@ color::color(unsigned char r, unsigned char g, unsigned char b)
     this->vr = r;
     this->vg = g;
     this->vb = b;
+    stringstream ss;
+    ss << "#" << hex << setfill('0') << setw(2) << (int)vr << setfill('0') << setw(2) << (int)vg << setfill('0') << setw(2) << (int)vb;
+    this->colorstr = ss.str();
 }
 
 color::color(std::string color)
 {
+    this->colorstr = color;
     color.erase(0, 1);
     int colorno = stoi(color, 0, 16);
     this->vr = colorno / 256 / 256;
@@ -42,9 +46,7 @@ unsigned char color::b() const
     return vb;
 }
 
-std::string color::string() const
+const std::string & color::string() const
 {
-    stringstream ss;
-    ss << "#" << hex << setfill('0') << setw(2) << (int)vr << setfill('0') << setw(2) << (int)vg << setfill('0') << setw(2) << (int)vb;
-    return ss.str();
+    return colorstr;
 }
--- color.hpp
+++ color.hpp
@@ -7,6 +7,7 @@ class color
 {
 private:
     unsigned char vr, vg, vb;
+    std::string colorstr;
 public:
     color();
     color(unsigned char r, unsigned char g, unsigned char b);
@@ -14,7 +15,7 @@ public:
     unsigned char r() const;
     unsigned char g() const;
     unsigned char b() const;
-    std::string string() const;
+    const std::string & string() const;
 };
 
 #endif //JEOPARDY_COLOR_H
