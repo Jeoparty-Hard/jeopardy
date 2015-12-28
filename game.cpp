555382a9d0ca5947638e8ad8a8a65a657993817a
Manuel Vögele <develop@manuel-voegele.de>
2015-12-28 15:18:06 +0100

    Send current state after disconnecting a player

--- game.cpp
+++ game.cpp
@@ -95,6 +95,9 @@ bool game::process_client_event(const GenericValue<UTF8<>> &event)
         if (!player.is_connected())
             throw jeopardy_exception("Player '" + player.get_name() + "' is not connected");
         player.disconnect();
+        Document d;
+        state->current_state(d);
+        data.server.broadcast(d);
         return false;
     }
     if (event_type == "refresh")
